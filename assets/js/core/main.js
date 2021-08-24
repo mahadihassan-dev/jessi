380926f6ebe0d04d884a0ce9dde54301628af9a0
Mahedi Hassan <mahadihassan.mhd@gmail.com>
2021-08-24 13:06:29 +0600

    Update

---
+++ assets/js/core/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // Trending Post Slider
+    $(".trending_slider").slick({
+        autoPlay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    })
+})
